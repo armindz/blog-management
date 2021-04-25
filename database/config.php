ff547ee91bddc72d1ed9b82fb5910d4268b6190b
Armin Dzibric <armindzibric6@gmail.com>
2021-04-25 12:53:44 +0200

    config

--- database/config.php
+++ database/config.php
@@ -1,5 +1,6 @@
 <?php  
-    define("DB_HOST", 'localhost');  
-    define("DB_USER", 'root');  
-    define("DB_PASSWORD", 'sifra');  
+    define("DB_HOST", 'eu-cdbr-west-01.cleardb.com');  
+    define("DB_USER", 'b9676aed960532');  
+    define("DB_PASSWORD", 'eadc3883');  
     define("DB_DATABASE", 'blogmanagement');  
+    // mysql://b9676aed960532:eadc3883@eu-cdbr-west-01.cleardb.com/heroku_610895e7fc2f6a6?reconnect=true
